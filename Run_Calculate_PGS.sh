35db8044acb8dcd5d9b1096ccd5af9675b9d11cc
Jared Balbona <63825329+JaredBalbona@users.noreply.github.com>
2023-03-20 13:59:47 -0600

    Update Run_Calculate_PGS.sh

--- Run_Calculate_PGS.sh
+++ Run_Calculate_PGS.sh
@@ -7,15 +7,11 @@ ml intel
 ml gcc
 
 for VA_Discovery in .2 .5 .8; do
-for AM_Discovery in 0 .05 .2 .8; do
+for AM_Discovery in 0 .2 .6 .8; do
 for VA_Target in .2 .5 .8; do
-for AM_Target in 0 .05 .2 .8; do
+for AM_Target in 0 .2 .6 .8; do
 
-file_path = "/path/to/main/directory/"
-sbatch "${file_path}/Discovery_Samples/GWAS/GeneEvolve_AM_GWAS.sh" "$VA" "$AM" 
-
-
-sbatch "${file_path}/Target_Samples/PGS/Calculate_PGS.sh" "$VA_Discovery" "$AM_Discovery" "$VA_Target" "$AM_Target"
+sbatch /pl/active/KellerLab/jared/Vertical_Transmission/GeneEvolve/Update_2023/Target_Samples/PGS/Calculate_PGS.sh "$VA_Discovery" "$AM_Discovery" "$VA_Target" "$AM_Target"
 
 done
 done
