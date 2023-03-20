b19580c5c17df0a9629b1fbc862ee9426f729e26
Jared Balbona <63825329+JaredBalbona@users.noreply.github.com>
2023-03-20 13:51:07 -0600

    Update Run_GeneEvolve_AM_GWAS.sh

--- Run_GeneEvolve_AM_GWAS.sh
+++ Run_GeneEvolve_AM_GWAS.sh
@@ -3,18 +3,14 @@
 #SBATCH --ntasks=1
 #SBATCH --time=0:15:00
 
-
 ml intel gcc
 
 for VA in .2 .5 .8; do
-for AM in 0 .05 .2 .8; do
+for AM in 0 .2 .6 .8; do
 
-file_path = "/path/to/main/directory/"
-
-sbatch "${file_path}/Discovery_Samples/GWAS/GeneEvolve_AM_GWAS.sh" "$VA" "$AM" 
+sbatch "/pl/active/KellerLab/jared/Vertical_Transmission/GeneEvolve/Update_2023/Discovery_Samples/GWAS/GeneEvolve_AM_GWAS.sh" "$VA" "$AM" 
 
 done
 done
-
 
 
