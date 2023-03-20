790cbcef8a520aa015b105410c84c5263f77af8a
Jared Balbona <63825329+JaredBalbona@users.noreply.github.com>
2023-03-20 14:00:55 -0600

    Update Compare_PGS_R2.R

--- Compare_PGS_R2.R
+++ Compare_PGS_R2.R
@@ -13,18 +13,18 @@ for params in params_list:
     phen_file = os.path.join(file_path, f"Sample_Phenotypes/{params}.info.pop1.gen10.sample")
     phen = pd.read_csv(phen_file, sep="\t", usecols=["ID", "ph1_P", "ph1_A"])[1:]
     phen["ID"] = "per" + phen["ID"].astype(str)
-#
+
     pgs_path = os.path.join(file_path, f"Sample_PGSs/{params}/")
     pgs_files = glob(os.path.join(pgs_path, "*.sscore"))
-#
+
     phen_pgs = phen
     for pgs_file in pgs_files:
         pgs_temp = pd.read_csv(pgs_file, sep="\t", usecols=[0, 3])
         pgs_name = re.search('Discovery_\\s*(.*?)\\s*.sscore', pgs_file).group(1)
         pgs_temp.columns = ["ID", pgs_name]
         phen_pgs = pd.merge(phen_pgs, pgs_temp, on="ID")
-#
-#
+
+
     new_phen_pgs = phen_pgs[['ID','ph1_A','ph1_P']]
     for param in params_list:
         # Sum across chromosomes:
@@ -34,10 +34,10 @@ for params in params_list:
         phen_pgs_temp = pd.DataFrame(phen_pgs_temp[['ID', param]])
         phen_pgs_temp.columns = ["ID", "PGS_" + param]
         new_phen_pgs = pd.merge(new_phen_pgs, phen_pgs_temp, on="ID")
-    #
+    
     minus_phen = new_phen_pgs.pop("ph1_P")
     new_phen_pgs.insert(1, "ph1_P", minus_phen)
-#
+
     # Save the Output:
     output_file = os.path.join(file_path, f"Phen_PGS/PGS_{params}.txt")
     new_phen_pgs.to_csv(output_file, sep=",", index=False)
@@ -46,10 +46,10 @@ for params in params_list:
 for params in params_list:
     input_file = os.path.join(file_path, f"Phen_PGS/PGS_{params}.txt")
     phen_pgs = pd.read_csv(input_file, sep=",")
-#
+
     phen_pgs_std = phen_pgs
     phen_pgs_std.iloc[:,1:] = (phen_pgs_std.iloc[:,1:] - phen_pgs_std.iloc[:,1:].mean()) / phen_pgs_std.iloc[:,1:].std()
-# 
+
     output_file_std = os.path.join(file_path, f"Phen_PGS/Standardized_PGS_{params}.txt")
     phen_pgs_std.to_csv(output_file_std, sep=",", index=False)
 
@@ -72,14 +72,14 @@ def regression_task(file):
     pgs_df = pgs_df.iloc[:, 1:]
     sim_params = re.search(r'PGS_(.*).txt', file).group(1)
     pgs_df = pgs_df.rename(columns={'ph1_A': 'True_VA', 'ph1_P': 'Phen_' + sim_params})
-#
+
     # Function to separately regress the phenotype on all other columns: 
     regressions = [round(np.corrcoef(pgs_df.iloc[:, 0], pgs_df[col])[0, 1] ** 2, 4) for col in pgs_df.columns[1:]]
     #
     # Put the regression results into the proper format
     results = pd.DataFrame({'Target': [pgs_df.columns[0]], 'True_VA': [regressions[0]], 
                             **{'PGS_' + params_list[i]: [regressions[i+1]] for i in range(len(params_list))}})
-#
+
     return results
 
 # create a list of data frames to perform the regression task on
