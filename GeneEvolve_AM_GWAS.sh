96aec16875c8bac491283af018a8f6f1dc026f35
Jared Balbona <63825329+JaredBalbona@users.noreply.github.com>
2023-03-20 13:50:48 -0600

    Update GeneEvolve_AM_GWAS.sh

--- GeneEvolve_AM_GWAS.sh
+++ GeneEvolve_AM_GWAS.sh
@@ -10,17 +10,15 @@ ml intel
 
 for chr in {1..22}; do
 
-file_path = "/path/to/main/directory/"
-mkdir "${file_path}/Discovery_Samples/GWAS/GWAS_Output/VA"$1"_VF0_matcor"$2
+mkdir "/pl/active/KellerLab/jared/Vertical_Transmission/GeneEvolve/Update_2023/Discovery_Samples/GWAS/GWAS_Output/VA"$1"_matcor"$2
 
 plink2 \
     --glm allow-no-covars \
     --const-fid \
-    --sample "${file_path}/Discovery_Samples/GWAS/GWAS_Input/VA"$1"_VF0_matcor"$2".info.pop1.gen10.sample" \
+    --sample "/pl/active/KellerLab/jared/Vertical_Transmission/GeneEvolve/Update_2023/Discovery_Samples/GWAS/GWAS_Input/VA"$1"_matcor"$2".info.pop1.gen10.sample" \
     --pheno-name "ph1_P" \
-    --haps "${file_path}/Discovery_Samples/GeneEvolve_Output/VA"$1"_VF0_matcor"$2"/VA"$1"_VF0_matcor"$2".pop1.gen10.chr" "$chr" ".hap" 'ref-first' \
-    --legend "${file_path}/sample.chr" "$chr" ".legend" "$chr" \
-    --out "${file_path}/Discovery_Samples/GWAS/GWAS_Output/VA"$1"_VF0_matcor"$2"/VA"$1"_VF0_matcor"$2".pop1.gen10.chr" "$chr"
+    --haps "/pl/active/KellerLab/jared/Vertical_Transmission/GeneEvolve/Update_2023/Discovery_Samples/GeneEvolve_Output/VA"$1"_matcor"$2"/VA"$1"_matcor"$2".pop1.gen10.chr"$chr".hap" 'ref-first' \
+    --legend "/pl/active/KellerLab/Emmanuel/pedofUKBphased/caucasians/5000/sample.chr"$chr".legend" "$chr" \
+    --out "/pl/active/KellerLab/jared/Vertical_Transmission/GeneEvolve/Update_2023/Discovery_Samples/GWAS/GWAS_Output/VA"$1"_matcor"$2"/VA"$1"_matcor"$2".pop1.gen10.chr"$chr
 
 done
-
