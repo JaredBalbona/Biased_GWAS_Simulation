68baabb13a18a577f032cefc06cbab6218f6b6bd
Jared Balbona <63825329+JaredBalbona@users.noreply.github.com>
2023-03-24 12:22:24 -0600

    Update Calculate_PGS.sh

--- Calculate_PGS.sh
+++ Calculate_PGS.sh
@@ -14,7 +14,7 @@ mkdir "/pl/active/KellerLab/jared/Vertical_Transmission/GeneEvolve/Update_2023/T
 
 plink2 \
     --haps /pl/active/KellerLab/jared/Vertical_Transmission/GeneEvolve/Update_2023/Target_Samples/GeneEvolve_Output/VA"$1"_matcor"$2"/VA"$1"_matcor"$2".pop1.gen10.chr"$chr".hap 'ref-first' \
-    --legend /pl/active/KellerLab/Emmanuel/pedofUKBphased/caucasians/5000/sample.chr"$chr".legend "$chr" \
+    --legend /pl/active/KellerLab/Emmanuel/pedofUKBphased/caucasians/52249/sample.chr"$chr".legend "$chr" \
     --score /pl/active/KellerLab/jared/Vertical_Transmission/GeneEvolve/Update_2023/Discovery_Samples/GWAS/GWAS_Output/VA"$3"_matcor"$4"/VA"$3"_matcor"$4".pop1.gen10.chr"$chr".ph1_P.glm.linear 3 4 9 header \
     --out /pl/active/KellerLab/jared/Vertical_Transmission/GeneEvolve/Update_2023/Target_Samples/PGS/Sample_PGSs/VA"$1"_matcor"$2"/Target_VA"$1"_matcor"$2"_Discovery_VA"$3"_matcor"$4"_chr"$chr"
 
