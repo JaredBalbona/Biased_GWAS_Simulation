4af0e0dbc2a8d302ffc02ca00f2282236b109fa8
Jared Balbona <63825329+JaredBalbona@users.noreply.github.com>
2023-03-24 12:22:39 -0600

    Update GeneEvolve_AM_GWAS.sh

--- GeneEvolve_AM_GWAS.sh
+++ GeneEvolve_AM_GWAS.sh
@@ -18,7 +18,7 @@ plink2 \
     --sample "/pl/active/KellerLab/jared/Vertical_Transmission/GeneEvolve/Update_2023/Discovery_Samples/GWAS/GWAS_Input/VA"$1"_matcor"$2".info.pop1.gen10.sample" \
     --pheno-name "ph1_P" \
     --haps "/pl/active/KellerLab/jared/Vertical_Transmission/GeneEvolve/Update_2023/Discovery_Samples/GeneEvolve_Output/VA"$1"_matcor"$2"/VA"$1"_matcor"$2".pop1.gen10.chr"$chr".hap" 'ref-first' \
-    --legend "/pl/active/KellerLab/Emmanuel/pedofUKBphased/caucasians/5000/sample.chr"$chr".legend" "$chr" \
+    --legend "/pl/active/KellerLab/Emmanuel/pedofUKBphased/caucasians/52249/sample.chr"$chr".legend" "$chr" \
     --out "/pl/active/KellerLab/jared/Vertical_Transmission/GeneEvolve/Update_2023/Discovery_Samples/GWAS/GWAS_Output/VA"$1"_matcor"$2"/VA"$1"_matcor"$2".pop1.gen10.chr"$chr
 
 done
